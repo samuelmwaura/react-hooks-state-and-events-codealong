ed54d2ae020bb832edd137dfa2efe6771d1a8eb1
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-16 03:09:16 +0300

    Completed the code along.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+  return <button onClick={()=>setIsOn(!isOn)} style={{background:isOn?'red':'white'}}>{isOn?'ON':'OFF'}</button>;
 }
 
 export default Toggle;
+//Heavy use oisOn?'red':'white of ternary expression for the comparison logic
